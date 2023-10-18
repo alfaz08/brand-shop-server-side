f84ae08f1b5b756b34222c58bdfaa44c6296a8e3
Alfaz Hossain <alfaz.cse@gmail.com>
2023-10-19 00:32:08 +0600

    product form is added

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors())
 app.use(express.json())
 
+
 app.get('/',(req,res)=>{
   res.send('brand shop server is running')
 })
@@ -17,8 +18,11 @@ app.listen(port,()=>{
   console.log(`server is running on PORT: ${port}`);
 })
 
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASS);
+
+
+
+
+
 
 
 
@@ -33,9 +37,32 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
+
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    //server name
+const productCollection = client.db('brandShopDB').collection('products')
+
+//store data in database
+
+app.post('/products',async(req,res)=>{
+  const newProduct = req.body;
+  console.log('new product',newProduct);
+  const result = await productCollection.insertOne(newProduct)
+  res.send(result)
+
+})
+
+//read data
+app.get('/products',async(req,res)=>{
+  const cursor=productCollection.find()
+  const result=await cursor.toArray();
+  res.send(result)
+})
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
